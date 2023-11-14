8258eaebaf8a9454b6143517010a1aaadea1abb5
Gabrielgo25 <josegabrielgonzalezmartinez@gmail.com>
2023-11-13 21:08:38 -0500

    Gabriel 13-11-23 9:08 pm

--- src/services/SoapServiceDirecciones.js
+++ src/services/SoapServiceDirecciones.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const BASE_REST_API_URL = "http://150.136.119.119:8285/api/v1/reporteDirecciones/filtrar";
-const BASE_REST_API_URL = "http://localhost:8285/api/v1/reporteDirecciones/filtrar";
+const BASE_REST_API_URL = "http://150.136.119.119:8285/api/v1/reporteDirecciones/filtrar";
+//const BASE_REST_API_URL = "http://localhost:8285/api/v1/reporteDirecciones/filtrar";
 
 class SoapServiceDirecciones {
     getAllDirecciones(custAccountID) {
